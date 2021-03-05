59cc5f5b08f12d467a983413155f97d00ceac620
shoun123 <61983269+shoun123@users.noreply.github.com>
2021-03-05 14:31:37 +0900

    ユーザー管理機能

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -11,7 +11,6 @@ FactoryBot.define do
     email { Faker::Internet.free_email }
     password = '1a' + Faker::Internet.password(min_length: 6)
 
-    password { password }
     password_confirmation { password }
 
   end
